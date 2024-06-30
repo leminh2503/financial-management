8d8e4e5261f733dece567a87b5f71ee74db33807
Minh Le <minh.lv@tinasoft.vn>
2024-06-30 21:30:08 +0700

    detail report Screen

--- screens/OtherScreen/DetailReportCategoryScreen/index.tsx
+++ screens/OtherScreen/DetailReportCategoryScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
   Box,
   Center,
@@ -8,8 +8,67 @@ import {
   Text,
   VStack,
 } from 'native-base';
+import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore';
+import { db } from '../../../hooks/useFirestorage';
+import { ITransaction } from '../../../types';
 
 const DetailReportCategoryScreen = () => {
+  const [transaction, setTransaction] = useState<ITransaction[]>([]);
+
+  async function getTransaction() {
+    try {
+      // Tạo query để lấy tài liệu từ collection "revenue" trong khoảng thời gian từ đầu tháng đến cuối tháng
+      const revenueQuery = query(collection(db, 'transaction'));
+      const revenueSnapshot = await getDocs(revenueQuery);
+
+      const revenueList = await Promise.all(
+        revenueSnapshot.docs.map(async (revenueDoc) => {
+          const revenueData = revenueDoc.data();
+          const categoryRef = revenueData.category;
+          // Lấy thông tin của category
+          let categoryData = {};
+          if (categoryRef) {
+            const categoryDoc = await getDoc(doc(db, categoryRef.path));
+            if (categoryDoc?.exists()) {
+              categoryData = categoryDoc?.data();
+            } else {
+              console.log('No such document!', revenueData);
+            }
+          }
+
+          return { ...revenueData, category: categoryData };
+        })
+      );
+      setTransaction(revenueList);
+    } catch (e) {
+      console.error('Error getting documents: ', e);
+    }
+  }
+
+  useEffect(() => {
+    getTransaction();
+  }, []);
+
+  const totalsRevenue = useMemo(() => {
+    return transaction
+      .filter((item) => item?.isRevenue)
+      .reduce((acc, cur) => {
+        return acc + cur?.amount;
+      }, 0);
+  }, [transaction]);
+
+  const totalsSpending = useMemo(() => {
+    return transaction
+      .filter((item) => !item?.isRevenue)
+      .reduce((acc, cur) => {
+        return acc + cur?.amount;
+      }, 0);
+  }, [transaction]);
+
+  const totals = useMemo(() => {
+    return (totalsSpending || 0) - (totalsRevenue || 0);
+  }, [totalsSpending, totalsRevenue]);
+
   return (
     <ScrollView bg="white">
       <Box mt={8} w="100%" maxW="100%" mx="auto">
@@ -17,17 +76,17 @@ const DetailReportCategoryScreen = () => {
           <VStack w="100%">
             <HStack justifyContent="space-between" bg="#F6F9FF" p={2}>
               <Text bold>Thu nhập</Text>
-              <Text bold>10,000,000đ</Text>
+              <Text bold>{totalsSpending}đ</Text>
             </HStack>
             <Divider />
             <HStack justifyContent="space-between" bg="#F6F9FF" p={2}>
               <Text bold>Chi tiêu</Text>
-              <Text bold>1,500,000đ</Text>
+              <Text bold>{totalsRevenue}đ</Text>
             </HStack>
             <Divider />
             <HStack justifyContent="space-between" bg="#F6F9FF" p={2}>
               <Text bold>Tổng</Text>
-              <Text bold>8,500,000đ</Text>
+              <Text bold>{totals}đ</Text>
             </HStack>
           </VStack>
           <VStack mt={4} w="100%">
@@ -38,7 +97,7 @@ const DetailReportCategoryScreen = () => {
             <Divider />
             <HStack justifyContent="space-between" bg="#F6F9FF" p={2}>
               <Text bold>Tổng</Text>
-              <Text bold>8,500,000đ</Text>
+              <Text bold>{totals}đ</Text>
             </HStack>
           </VStack>
         </Center>
