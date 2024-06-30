a81771f0995140bdaf48eb2db3f444fcbece4673
Minh Le <minh.lv@tinasoft.vn>
2024-06-30 21:39:47 +0700

    total screens

--- screens/OtherScreen/ReportTotalScreen/Expense/index.tsx
+++ screens/OtherScreen/ReportTotalScreen/Expense/index.tsx
@@ -1,43 +1,78 @@
-import { View } from 'react-native';
-import { Box, Divider, HStack, Icon, Text } from 'native-base';
-import { FontAwesome5 } from '@expo/vector-icons';
-import React from 'react';
+import { Image, View } from 'react-native';
+import { Box, Divider, HStack, Text } from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { ITransaction } from '../../../../types';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from '../../../../hooks/useFirestorage';
 
-const transactions = [
-  { category: 'Ăn uống', amount: '1,000,000đ', type: 'expense' },
-  { category: 'Chi tiêu hàng ngày', amount: '500,000đ', type: 'expense' },
-];
+export const ExpensesCategory = () => {
+  const [transaction, setTransaction] = useState<ITransaction[]>([]);
 
-export const ExpensesCategory = () => (
-  <View style={{ flex: 1, padding: 16 }}>
-    {transactions.map((transaction, index) => (
-      <Box key={index}>
-        <HStack justifyContent="space-between" alignItems="center">
-          <HStack alignItems="center">
-            <Icon
-              as={FontAwesome5}
-              name={
-                transaction.category === 'Ăn uống'
-                  ? 'utensils'
-                  : 'shopping-cart'
-              }
-              size="sm"
-              color={
-                transaction.category === 'Ăn uống' ? 'orange.500' : 'green.500'
-              }
-              mr={2}
-            />
-            <Text bold>{transaction.category}</Text>
+  async function getRevenue() {
+    try {
+      // Tạo query để lấy tài liệu từ collection "revenue" trong khoảng thời gian từ đầu tháng đến cuối tháng
+      const revenueQuery = query(
+        collection(db, 'transaction'),
+        where('isRevenue', '==', true)
+      );
+      const revenueSnapshot = await getDocs(revenueQuery);
+
+      const revenueList = await Promise.all(
+        revenueSnapshot.docs.map(async (revenueDoc) => {
+          const revenueData = revenueDoc.data();
+          const categoryRef = revenueData.category;
+          // Lấy thông tin của category
+          let categoryData = {};
+          if (categoryRef) {
+            const categoryDoc = await getDoc(doc(db, categoryRef.path));
+            if (categoryDoc?.exists()) {
+              categoryData = categoryDoc?.data();
+            } else {
+              console.log('No such document!', revenueData);
+            }
+          }
+
+          return { ...revenueData, category: categoryData };
+        })
+      );
+      setTransaction(revenueList);
+    } catch (e) {
+      console.error('Error getting documents: ', e);
+    }
+  }
+
+  useEffect(() => {
+    getRevenue();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      {transaction.map((item, index) => (
+        <Box key={index}>
+          <HStack justifyContent="space-between" alignItems="center">
+            <HStack alignItems="center">
+              <Image
+                source={{ uri: item?.category?.image }}
+                style={{ width: 24, height: 24, paddingBottom: 8 }}
+              />
+              <Text bold mx={2}>
+                {item.category.title}
+              </Text>
+            </HStack>
+            <Text bold color={'red.500'}>
+              {item.amount}đ
+            </Text>
           </HStack>
-          <Text
-            bold
-            color={transaction.type === 'expense' ? 'red.500' : 'blue.500'}
-          >
-            {transaction.amount}
-          </Text>
-        </HStack>
-        <Divider my={2} />
-      </Box>
-    ))}
-  </View>
-);
+          <Divider my={2} />
+        </Box>
+      ))}
+    </View>
+  );
+};
